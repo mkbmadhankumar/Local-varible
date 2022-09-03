f3d4aea95f4998c6af8aed9555c790486f7b8170
mkbmadhankumar <mkbmadhankumar@gmail.com>
2022-09-03 16:07:54 +0530

    message

---
+++ src/org/Employee/Java.java
@@ -0,0 +1,25 @@
+package org.Employee;
+
+public class Java {
+	
+	//Local varible
+	{int b=0;
+System.out.println(b);
+}
+ 
+ 
+private void empld(int c) {
+	//Local varible
+	int a=100;
+			System.out.println("empld id 123456");
+	System.out.println(a);
+System.out.println(c);
+}
+
+
+
+public static void main (String args []) {
+	Java java=new Java();
+	java.empld(300);
+}
+}
